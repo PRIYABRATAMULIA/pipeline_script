9393a475b515268b9698da882b623963e142bd5e
priyabrata.mulia.thbs <priyabrata_mulia@thbs.com>
2019-12-19 19:29:08 +0530

    File added

---
+++ project2.py
@@ -0,0 +1,8 @@
+#list = [10,11,12,13,14,15]
+n=1
+for x in range(1, 100):
+    if ((n % x) == 0) and ((n % x) == 0):
+        print ("This is not a Prime number" , n)
+        continue
+    else:
+        print ("This is a prime number", n)
