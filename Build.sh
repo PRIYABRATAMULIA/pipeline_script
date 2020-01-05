19b33b134438dcb0a19500d3a1164f99a9aa546f
Priyabrata Mulia <priyabratamulia@gmail.com>
2020-01-06 03:08:31 +0530

    Add files via upload

--- Build.sh
+++ Build.sh
@@ -1,1 +1,8 @@
-echo "Building the Project : %date% : %time%"
+echo "Maven build initiated";
+sleep 2;
+echo "Maven Build started";
+sleep 2;
+echo "...........................................";
+sleep 2;
+echo "Package is ready";
+date
--- Quality.sh
+++ Quality.sh
@@ -1,1 +1,8 @@
-echo "Quality Gate Check : %date% : %time%"
+echo "Quality Gate Check : Is in progress";
+sleep 2;
+echo "...................................."
+sleep 2;
+echo "...................................."
+sleep 2;
+echo "Quality gate Check : Completed";
+date
