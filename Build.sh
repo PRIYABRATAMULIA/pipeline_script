c526d0276a612ad7d092ffa8c0b752c0a3cbfc08
priyabrata.mulia.thbs <priyabrata_mulia@thbs.com>
2020-01-07 01:45:25 +0530

    Added

---
+++ Build.sh
@@ -0,0 +1,8 @@
+echo "Maven build initiated";
+sleep 2;
+echo "Maven Build started";
+sleep 2;
+echo "...........................................";
+sleep 2;
+echo "Package is ready";
+date
---
+++ Deploy.sh
@@ -0,0 +1,12 @@
+echo "Deploying Build started";
+sleep 2;
+echo "Deploying build on web server in in progress";
+sleep 2;
+echo ".............................................";
+sleep 2;
+echo ".............................................";
+sleep 2;
+echo ".............................................";
+sleep 2;
+echo "Deployment on web server completed";
+date
